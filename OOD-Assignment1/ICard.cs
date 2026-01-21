7ebfe86bb4f97501d7b5d59a8bd039f05a754ac8
Kataali N <kataalinashiru@gmail.com>
2026-01-21 17:45:42 -0500

    Properties and methods for the ICard interface decided

--- OOD-Assignment1/ICard.cs
+++ OOD-Assignment1/ICard.cs
@@ -8,6 +8,22 @@ namespace OOD_Assignment1
 {
     interface ICard
     {
+        //start with properties needed for all cards
+        string rank { get; }
+        int Suit {  get; }
 
+        //define methods all cards would need to implement
+        bool ranksMatch();
+        bool suitsMatch();
+        bool isEightCard();
+        
+    }
+
+    //define an enum to hold the suit
+    enum Suit{
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades
     }
 }
