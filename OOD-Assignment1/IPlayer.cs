cc15b27827a7525360c3b5f69b83086013b6cd29
Kataali N <kataalinashiru@gmail.com>
2026-02-06 13:04:52 -0500

    Edited the name of the draw method in IPlayer for clarity, and added one more method to deal cards at the start of the game

--- OOD-Assignment1/IPlayer.cs
+++ OOD-Assignment1/IPlayer.cs
@@ -12,7 +12,8 @@ namespace OOD_Assignment1
 
         void takeTurn(); //decides which player's turn it is
         void playCard(); // requires player to play their turn
-        void drawCard(); //requires player to add a card to their hand when they can't match suit or deck
+        void drawCardFromDeck(); //requires player to add a card to their hand when they can't match suit or deck
         bool hasWon { get; } // determines when a player wins
+        void receiveCard(ICard card); //used to deal cards at the start of the game, allows for drawn card to be added to the player's hand
     }
 }
